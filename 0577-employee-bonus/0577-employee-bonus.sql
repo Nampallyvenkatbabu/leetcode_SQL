365f84c5d57136d80752fabde318233100b1582e
Nampally Venkatbabu <116674069+Nampallyvenkatbabu@users.noreply.github.com>
2026-07-12 12:32:45 +0530

    Time: 1175 ms (19.71%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,30 @@
+# Write your MySQL query statement below
+/*
+select e.name,b.bonus 
+from Employee e
+LEFT JOIN
+Bonus b
+on e.empId = b.empId 
+where b.bonus < 1000 or b.bonus is null
+
+Alternate solution for corelated subquery */
+
+select e.name,
+(
+    select b.bonus 
+    from Bonus b
+    where b.empId = e.empId 
+) as bonus
+from Employee e
+where 
+(
+    select b.bonus 
+    from Bonus b
+    where b.empId = e.empId 
+) < 1000
+or 
+(
+    select b.bonus 
+    from Bonus b
+    where b.empId = e.empId 
+) is NULL;
