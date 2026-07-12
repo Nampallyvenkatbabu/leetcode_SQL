5c9da3c77f640555a31b53b04a44b0cfb80ff39a
Nampally Venkatbabu <116674069+Nampallyvenkatbabu@users.noreply.github.com>
2026-07-12 13:14:43 +0530

    Time: 342 ms (99.68%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -7,3 +7,16 @@ JOIN
 Employee m
 on e.managerId = m.id
 where e.salary > m.salary
+
+/*
+Alternative solution with corelated subquery
+
+select e.name as Employee 
+from Employee e
+where e.salary > (
+    select m.salary
+    from Employee m
+    where m.id = e.managerId 
+)
+
+*/
